83bcade1c651d91f0c29c65c08cef0faa2c27f15
hashcowuwu <3189506476@qq.com>
2024-06-03 18:44:45 +0800

    Completed the first exercise

--- ex1/lock2/ex1.c
+++ ex1/lock2/ex1.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
-#include <wchar.h>
+#include <pthread.h>
+#include <time.h>
 
-
+#define N 10000000
 unsigned long a = 0;
 
+pthread_mutex_t global_lock;
+
+void *routine(void *arg) {
+  for (int i = 0 ; i < N ; ++ i) {
+    while(pthread_mutex_trylock(&global_lock) != 0);
+    a ++;
+    pthread_mutex_unlock(&global_lock);
+  }
+  return NULL;
+}
 
 int main() {
 
+  pthread_mutex_init(&global_lock, NULL);
+  pthread_t threads[2];
+
+  for (int i =  0 ; i < 2 ; ++ i )
+    pthread_create(&threads[i], NULL, routine, 0);
+
+  for (int i = 0 ; i < 2  ; ++ i ) 
+    pthread_join(threads[i],NULL);
+
+  printf("%lu\n",a);
+
+  return 0;
 }
