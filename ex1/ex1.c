8f3430aadd9fd86d11b9ce5f8bd8d2692a90480b
Hashcatuwu <3189506476@qq.com>
2024-06-03 13:39:10 +0800

    abc

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <time.h>
 
 #define N 10000000
 unsigned long a = 0;
@@ -12,4 +13,15 @@ void *routine(void *arg) {
 
 int main() {
 
+  
+  pthread_t threads[2];
+  for (int i =  0 ; i < 2 ; ++ i )
+    pthread_create(&threads[i], NULL, routine, 0);
+
+  for (int i = 0 ; i < 2  ; ++ i ) 
+    pthread_join(threads[i],NULL);
+
+  printf("%lu\n",a);
+
+  return 0;
 }
