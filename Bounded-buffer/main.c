9b03b3c7b734d79c0878f647e32c5546cc04333b
hashcowuwu <3189506476@qq.com>
2024-06-14 13:47:51 +0800

    Completed the first exercise

--- Bounded-buffer/main.c
+++ Bounded-buffer/main.c
@@ -2,6 +2,56 @@
 #include <pthread.h>
 
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+int buffer_has_data = 0;
+
+
+void* producer(void* arg){
+
+    printf("<");
+
+    pthread_mutex_lock(&mutex);
+   
+    buffer_has_data = 1;
+
+    pthread_cond_signal(&cond);
+
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+    
+}
+
+
+void* consumer(void* arg){
+    pthread_mutex_lock(&mutex);
+
+    while(buffer_has_data == 0){
+        pthread_cond_wait(&cond, &mutex);
+    }
+
+    printf(">");
+
+    buffer_has_data = 0;
+
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}
+
+
 int main(){
+    pthread_t pthreads[2];
+    pthread_t bthreads[2];
+    for (int i = 0 ; i < 2 ; ++ i ) {
+        pthread_create(&pthreads[i],NULL,producer,0);
+        pthread_create(&bthreads[i],NULL,consumer,0);
+    }
+    for (int i = 0 ; i < 2 ; ++ i ) {
+        pthread_join(pthreads[i], NULL);
+        pthread_join(bthreads[i], NULL);
+    }
 
 }
